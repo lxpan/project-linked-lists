e1f9c65d9dd847f8cb1cf447e76356878face2c2
Luoxi Pan <l.panpax@gmail.com>
2022-10-24 23:00:51 +1100

    Remove commented lines

--- Node.js
+++ Node.js
@@ -1,13 +1,9 @@
-export default function NodeFactory(_value=null, _nextNode=null) {
+export default function NodeFactory(_value = null, _nextNode = null) {
     const value = _value;
-    // const value = () => {
-    //     return _value;
-    // }
-
     const nextNode = _nextNode;
 
     return {
         value,
-        nextNode
-    }
+        nextNode,
+    };
 }
