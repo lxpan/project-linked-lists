ac8805327d80ab05111d31b143e88f5618f00c5e
Luoxi Pan <l.panpax@gmail.com>
2022-10-24 18:30:36 +1100

    Create functions append, prepend and printList

--- LinkedList.js
+++ LinkedList.js
@@ -2,22 +2,55 @@ import NodeFactory from './Node.js';
 
 class LinkedList {
     constructor() {
-        this.head = NodeFactory();
+        this.head = NodeFactory('HEAD');
         this.length = 0;
+        this.current = null;
+    }
+
+    append(value) {
+        this.current = this.head;
+
+        while(this.current.nextNode !== null) {
+            // console.log(`Value: ${this.current.value}`);
+            // console.log(`Next node: ${this.current.nextNode.value}`);
+            this.current = this.current.nextNode;
+        }
+
+        if (this.current.nextNode == null) {
+            // console.log(`Value: ${this.current.value}`);
+            // console.log(`Next node: ${this.current.nextNode.value}`);
+            // this.current.value = value;
+            this.current.nextNode = NodeFactory(value);
+            this.length++;
+        }
     }
 
     prepend(value) {
-        const newNode = NodeFactory(value, this.head);
-        this.head = newNode;
+        const newNode = NodeFactory(value, this.head.nextNode);
+        this.head.nextNode = newNode;
         this.length++;
+    }
+
+    printList() {
+        this.current = this.head;
+        console.log(this.current);
+
+        while(this.current.nextNode !== null) {
+            console.log(`${this.current.value}-->${this.current.nextNode.value}`);
+            this.current = this.current.nextNode;
+        }
+
+        console.log(`${this.current.value}-->${this.current.nextNode}`);
     }
 }
 
 const myList = new LinkedList();
+myList.append('DouDou');
+myList.append('Mean Cat');
 myList.prepend('John');
-myList.prepend('Johannes');
-myList.prepend('Johanna');
-console.log(myList);
+
+myList.printList();
+
 
 
 
